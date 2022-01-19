2d589b35714b4a6e0d77f77c0b4eaeafe15e1dc7
Luke Nguyen <lukenguyen.dev@gmail.com>
2022-01-18 20:37:02 -0500

    features: added changes based on feedback
    	* installed tailwindcss/forms to normalize form styling
    	* added more config for tailwind: primary & secondary colors, font family

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    colors: {
+      primary: '#f06c9b',
+      secondary: '#b0228c',
+    },
   },
   plugins: [],
 };
