0be47f6390bfee8f9eac65d3119cdf06169e99d5
Luke Nguyen <59778930+mqnguyen5@users.noreply.github.com>
2022-02-07 16:39:11 -0500

    features/34-build-profile-page (#146)
    * features/34-build-profile-page
    	* installed lightGallery.js
    	* added a user profile "card" with avatar, name, age, bio, etc.
    	* added a photo gallery with zoom option when clicked
    
    * feature: add profile route
    
    Co-authored-by: Luke Nguyen <lukenguyen.dev@gmail.com>
    Co-authored-by: Minh Hang Nguyen <hang00103@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,48 +5,51 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/index";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 import Match from "./pages/Match/Match";
+import ProfilePage from "./pages/ProfilePage/index";
 
 function App() {
-	// const { loading, error, data } = useQuery(GET_TRACKS);
+  // const { loading, error, data } = useQuery(GET_TRACKS);
 
-	// if (loading) return 'Loading...';
-	// if (error) return `Error! ${error.message}`;
+  // if (loading) return 'Loading...';
+  // if (error) return `Error! ${error.message}`;
 
-	// return (
-	//   <div className="App">
-	//     <header className="App-header">
-	//       <img src={logo} className="App-logo" alt="logo" />
-	//       <p>
-	//         Edit <code>src/App.js</code> and save to reload.
-	//       </p>
-	//       <a
-	//         className="App-link"
-	//         href="https://reactjs.org"
-	//         target="_blank"
-	//         rel="noopener noreferrer"
-	//       >
-	//         Learn React
-	//       </a>
-	//     </header>
-	//     <div>
-	//       <ul>
-	//         {data.tracksForHome.map((track) => (
-	//           <li>{track.title}</li>
-	//         ))}
-	//       </ul>
-	//     </div>
-	//   </div>
-	// );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //     <div>
+  //       <ul>
+  //         {data.tracksForHome.map((track) => (
+  //           <li>{track.title}</li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   </div>
+  // );
 
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LandingPage />} />
-				<Route path="/reset-password" element={<ResetPassword />} />
-				<Route path="/match" element={<Match />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/match" element={<Match />} />
+        {/* need to update the profile route to take user id as param */}
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
