f636821604c85a9dba1cca07f000205fd8fc2c7d
Vivian Vu <44437742+hlavu@users.noreply.github.com>
2022-02-16 16:25:09 -0500

    feature: build report UI (#195)

--- frontend/src/components/Input/Label.js
+++ frontend/src/components/Input/Label.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Label = ({ id, label, error, required = false }) => {
+const Label = ({ id, label, error, required = false, addStyle }) => {
   return (
     <div>
-      <label id={id} className={`text-medium-gray font-bold`}>
+      <label id={id} className={`text-medium-gray font-bold ${addStyle}`}>
         {label} {required && <span className="text-primary">*</span>}
       </label>
       {error && <p className={`text-primary text-xs `}>{error}</p>}
